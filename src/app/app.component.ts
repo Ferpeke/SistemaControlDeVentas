4ccbc965c7c7f191e6ef1c20131ad8c5212d2d41
Fernando Sebastian <ferna9752@gmail.com>
2024-06-06 12:05:55 -0600

    Importamos los componentes ya creados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, NavBarComponent, ListadoClientesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
