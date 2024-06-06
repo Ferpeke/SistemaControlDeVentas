c2136bc857d92db122e50600e5dd5a6873760f68
Fernando Sebastian <ferna9752@gmail.com>
2024-06-06 12:39:24 -0600

    Incluimos en HttpClient para hacer las peticiones hacia la API

---
+++ src/app/clientes-service.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesServiceService {
+
+  constructor(private http: HttpClient) { 
+
+    // Metodos 
+
+    // Metodo que al realizar una peticion get al API, para obtener la lista de clientes
+    // Método que hace lo que anteriormente haciamos en HTTPie
+    obtenerListaClientes(): void {
+      this.http.get('http://localhost:8082/api/clientes').subscribe((respuesta : any) => {
+        console.log(respuesta);
+      });
+    }
+  }
+}
