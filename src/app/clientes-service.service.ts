4b3f8c4d5d35eabf514ad05e724802bfe77a2120
Fernando Sebastian <ferna9752@gmail.com>
2024-06-06 13:24:59 -0600

    Pintamos la Api

--- src/app/clientes-service.service.ts
+++ src/app/clientes-service.service.ts
@@ -6,9 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class ClientesServiceService {
   // Atributos
-  // public urlService: string = "http://localhost:8082/"; // Ambiente local
-  public urlService: string = "http://10.10.10.36:8082/"; // Ambiente local
-  constructor(private http: HttpClient) {  }
+  public urlService: string = "http://localhost:8082/"; // Ambiente local
+  //public urlService: string = "http://10.10.10.36:8082/"; // Ambiente Externo
+  public listaClientes: any[];
+  constructor(private http: HttpClient) { 
+    this.listaClientes = [];
+   }
   // TODO: Metodos 
 
     // Metodo que al realizar una peticion get al API, para obtener la lista de clientes
@@ -16,6 +19,7 @@ export class ClientesServiceService {
     obtenerListaClientes(): void {
       this.http.get(this.urlService + 'api/clientes').subscribe((respuesta : any) => {
         console.log(respuesta);
+        this.listaClientes = respuesta;
       });
     }
 }
--- src/app/listado-clientes/listado-clientes.component.ts
+++ src/app/listado-clientes/listado-clientes.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { ClientesServiceService } from '../clientes-service.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-listado-clientes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './listado-clientes.component.html',
   styleUrl: './listado-clientes.component.css'
 })
 export class ListadoClientesComponent {
 
-  constructor(cs : ClientesServiceService){
+  constructor(private cs : ClientesServiceService){ // añadimos un modificador de acceso
     cs.obtenerListaClientes();
   }
-  
+
+  // TODO: Nos traemos el listado clientes
+  //gets
+  get listado(){
+    return this.cs.listaClientes;
+  }
+
 }
