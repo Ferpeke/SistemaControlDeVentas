ae16f9a7a8ff4f65d0e551dbc314a7717fd9f406
Fernando Sebastian <ferna9752@gmail.com>
2024-06-07 10:03:15 -0600

    Creación de la rutas: app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
+import { AltaClienteComponent } from './alta-cliente/alta-cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "clientes", component: ListadoClientesComponent},
+    {path : "clientes/alta", component: AltaClienteComponent},
+    {path : "", redirectTo : "/clientes", pathMatch : "full"}
+];
