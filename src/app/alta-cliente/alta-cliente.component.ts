5ea2eac0347e9bba187e49acc9d61434bf6b4e95
Fernando Sebastian <ferna9752@gmail.com>
2024-06-07 09:00:22 -0600

    Creacion del metodo que recupera los datos del formulario

--- src/app/alta-cliente/alta-cliente.component.ts
+++ src/app/alta-cliente/alta-cliente.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ClientesServiceService } from '../clientes-service.service';
 
 @Component({
   selector: 'app-alta-cliente',
@@ -8,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrl: './alta-cliente.component.css'
 })
 export class AltaClienteComponent {
+  // Atributos
+  @ViewChild("nombre")
+  referenciaNombre!: ElementRef;
+
+  @ViewChild("apPaterno")
+  referenciaApPaterno!: ElementRef;
+
+  @ViewChild("apMaterno")
+  referenciaApMaterno!: ElementRef;
+
+  @ViewChild("telefono")
+  referenciaTelefono!: ElementRef;
+
+  @ViewChild("correo")
+  referenciaCorreo!: ElementRef;
+
+  @ViewChild("numAfiliado")
+  referenciaNumAfiliado!: ElementRef;
+
+  constructor(private servicio : ClientesServiceService){}
+
+  // TODO: metodo
+  guardarCliente() {
+    // Recuperamos los valores que vienen en las cajas
+    const nombre = this.referenciaNombre.nativeElement.value;
+    const apPaterno = this.referenciaApPaterno.nativeElement.value;
+    const apMaterno = this.referenciaApMaterno.nativeElement.value;
+    const telefono = this.referenciaTelefono.nativeElement.value;
+    const correo = this.referenciaCorreo.nativeElement.value;
+    const numAfiliado = this.referenciaNumAfiliado.nativeElement.value;
+    this.servicio.guardarCliente(nombre, apPaterno, apMaterno, telefono, correo, numAfiliado);
+  }
 
 }
