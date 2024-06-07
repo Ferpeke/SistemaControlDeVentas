29d801d87f1e34d9bc7764091b2879c6da48c51e
Fernando Sebastian <ferna9752@gmail.com>
2024-06-07 08:40:53 -0600

    Corrección y lo importamos en el componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ListadoClientesComponent } from './listado-clientes/listado-clientes.component';
+import { AltaClienteComponent } from './alta-cliente/alta-cliente.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, NavBarComponent, ListadoClientesComponent],
+  imports: [RouterOutlet, FooterComponent, NavBarComponent, ListadoClientesComponent,
+    AltaClienteComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
