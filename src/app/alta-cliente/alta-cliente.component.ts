0f997f9c79002b57cc40dc42d28cd10808efe4d1
Fernando Sebastian <ferna9752@gmail.com>
2024-06-07 08:27:28 -0600

    Creacion del componente AltaCliente

---
+++ src/app/alta-cliente/alta-cliente.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alta-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './alta-cliente.component.html',
+  styleUrl: './alta-cliente.component.css'
+})
+export class AltaClienteComponent {
+
+}
